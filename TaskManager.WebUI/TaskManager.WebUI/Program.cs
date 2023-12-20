bcc9f34b9e2a5e0c1e55ef2608796162bff03061
A.Osavoliuk <sasha.osavoll11@gmail.com>
2023-12-20 12:27:04 +0200

    Add project files.

---
+++ TaskManager.WebUI/TaskManager.WebUI/Program.cs
@@ -0,0 +1,29 @@
+using TaskManager.WebUI.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(TaskManager.WebUI.Client._Imports).Assembly);
+
+app.Run();
